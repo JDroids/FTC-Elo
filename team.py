99dce114fefa14d62d21e616f0fd2f35ac705ad5
JDroids <jdroids7026@gmail.com>
2018-06-09 17:58:36 -0400

    Removed non-functional method expected() from teams.py

--- team.py
+++ team.py
@@ -7,13 +7,7 @@ class Team:
     def __init__(self, team_number: int, team_name: str):
         self.team_number = team_number
         self.team_name = team_name
-
-    def expected(self, teammate, opponent_1, opponent_2):
-        alliance_average_rating = (self.ranking + teammate.ranking) / 2
-        opponent_alliance_average_rating = (opponent_1.ranking + opponent_2.ranking) / 2
-
-        return 1 / 10**(opponent_alliance_average_rating-alliance_average_rating)/400
-
+        
     def update_ranking(self, alliance_score: int, opponent_alliance_score: int, teammate, opponent_1, opponent_2):
         opponent_alliance_average_rating = (opponent_1.ranking + opponent_2.ranking) / 2
 
